9b5f7553a089c041e60e4d0fd4ba2af90ab4f46d
lucky529 <1248327663@qq.com>
2018-10-09 16:47:21 +0800

    提交代码

---
+++ 10.9/10.9/test.c
@@ -0,0 +1,72 @@
+//#include<stdio.h>
+//int year(int y)
+//{
+//	
+//
+//	if((0==y%4&&0!=y%100)||(0==y%400))
+//	return 1;
+//
+//}
+//int main()
+//{
+//	int y=0;
+//	printf("输入一个年份\n");
+//	scanf("%d",&y);
+//	year(y);
+//	if(year(y)==1)
+//	{
+//		printf("是闰年\n");
+//	}
+//	else
+//	{
+//		printf("不是闰年\n");
+//	}
+//
+//
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("gg\n");
+//return 0;
+//}
+//#include<stdio.h>
+//int is_sushu(int num)
+//{
+//	int i=0;
+//	for(i=2; i<num/2; i++)
+//	{
+//		if(num%i==0)
+//			break;
+//	}
+//if(i<num/2)
+//{
+//	return 0;
+//}
+//else
+//{
+//	return 1;
+//}
+//
+//
+//
+//}
+//int main()
+//{
+//	int num=0;
+//	printf("请输入一个数\n");
+//	scanf("%d",&num);
+//	is_sushu(num);
+//	if(1==is_sushu(num))
+//	{
+//		printf("这个数是素数\n");
+//	}
+//	else
+//	{
+//		printf("这个数不是素数\n");
+//	}
+//
+//
+//return 0;
+//}
