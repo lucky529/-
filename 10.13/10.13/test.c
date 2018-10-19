e57716c70f2b8e14e344fe2f5bac9aaa5cd576c8
lucky529 <1248327663@qq.com>
2018-10-19 22:29:34 +0800

    提交代码

--- 10.13/10.13/test.c
+++ 10.13/10.13/test.c
@@ -1,47 +1,18 @@
-#include"game.h"
-
-
-void menu()
+#include<stdio.h>
+int my_strlen(char *string)
 {
-	printf("********************\n");
-	printf("*****1.游戏开始*****\n");
-	printf("*****0.游戏退出*****\n");
-	printf("********************\n");
+    int count = 0;
+    if (*string == ‘\0’)
+    {
+        return 0;
+    }
+    return 1+my_strlen(char *string+1)
 }
 
-void game()
-{
-	char mine[ROWS][COLS]={0};
-	char show[ROWS][COLS]={0};
-	InitBoard(mine,ROWS,COLS,'0');
-	InitBoard(show,ROWS,COLS,'*');
-	DisplayBoard(show,ROW,COL);
-	Setmine(mine,ROW,COL);
-	DisplayBoard(mine,ROW,COL);
-
-	FindMine(mine,show,ROW,COL);
-}
 int main()
 {
-	int input;
-	srand((unsigned int)time(NULL));
-	do
-	{
-		menu();
-		scanf("%d",&input);
-
-		switch(input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("游戏退出\n");
-			break;
-		default:
-			printf("输入有误\n");
-			break;
-		}
-	}while(input);
-	return 0;
+    char *str = "abcdef";
+    printf("%d\n",my_strlen(str));
+    system("pause");
+    return 0;
 }
