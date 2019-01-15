627ac6b92f0aa04d5e346bbda434cfc0d1d6b935
lucky529 <1248327663@qq.com>
2019-01-15 19:27:31 +0800

    日期类

--- 单链表面试题3/单链表面试题3/test.c
+++ 单链表面试题3/单链表面试题3/test.c
@@ -118,3 +118,52 @@ struct ListNode *detectCycle(struct ListNode *head) {
 		return Union;
 	}
 }
+typedef struct RandomListNode RLNode;
+struct RandomListNode *copyRandomList(struct RandomListNode *head)//复制带随机指针的链表
+{
+	if (head == NULL) return head;
+	RLNode* cur = head;
+	while (cur)
+	{
+		RLNode* next = cur->next;
+		RLNode* copy = (RLNode*)malloc(sizeof(RLNode));
+		copy->label = cur->label;
+		cur->next = copy;
+		copy->next = next;
+
+		cur = next;
+	}
+	cur = head;
+	while (cur)
+	{
+		RLNode* copy = cur->next;
+		if (cur->random)
+		{
+			copy->random = cur->random->next;
+		}
+		else
+		{
+			copy->random = NULL;
+		}
+
+		cur = copy->next;
+	}
+	RLNode* newhead, *tail;
+	newhead = tail = (RLNode*)malloc(sizeof(RLNode));
+	cur = head;
+	while (cur)
+	{
+		RLNode* copy = cur->next;
+		RLNode* next = copy->next;
+
+		tail->next = copy;
+		tail = tail->next;
+
+		cur->next = next;
+		cur = next;
+	}
+	RLNode* Nhead = newhead->next;
+	free(newhead);
+
+	return Nhead;
+}
